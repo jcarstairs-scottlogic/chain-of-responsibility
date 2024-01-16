72586add8db3ce1b22b523156eda3b0d4788a53b
Joe Carstairs <jcarstairs@scottlogic.com>
2024-01-16 10:59:34 +0000

    Fix NotTranslatableException

--- src/main/java/com/scottlogic/cor/exceptions/NotTranslatableException.java
+++ src/main/java/com/scottlogic/cor/exceptions/NotTranslatableException.java
@@ -1,8 +1,21 @@
-package com.scottlogic.cor;
+package com.scottlogic.cor.exceptions;
 
 public class NotTranslatableException extends RuntimeException {
+    private String lang;
+    private String text;
+
     public NotTranslatableException(String lang, String text) {
         super("Could not translate " + text + " from " + lang);
+        this.lang = lang;
+        this.text = text;
+    }
+
+    public String getLang() {
+        return this.lang;
+    }
+
+    public String getText() {
+        return this.text;
     }
 }
 
