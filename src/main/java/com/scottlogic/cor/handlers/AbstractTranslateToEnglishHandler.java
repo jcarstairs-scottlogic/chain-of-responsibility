1375dddc385c3d29b4eb7df4d5ef7ee24c81f51a
Joe Carstairs <jcarstairs@scottlogic.com>
2024-01-16 12:02:49 +0000

    Initialises next handler to empty Optional instead of null

--- src/main/java/com/scottlogic/cor/handlers/AbstractTranslateToEnglishHandler.java
+++ src/main/java/com/scottlogic/cor/handlers/AbstractTranslateToEnglishHandler.java
@@ -3,7 +3,7 @@ package com.scottlogic.cor.handlers;
 import java.util.Optional;
 
 public abstract class AbstractTranslateToEnglishHandler implements TranslateToEnglishHandler {
-    protected Optional<TranslateToEnglishHandler> next;
+    protected Optional<TranslateToEnglishHandler> next = Optional.empty();
 
     public void setNext(TranslateToEnglishHandler next) {
         this.next = Optional.of(next);
